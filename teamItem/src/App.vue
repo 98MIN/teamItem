d29bc6c8af5b95d96f0d30c9f94564fb92d3c529
98MIN <1033312504@qq.com>
2018-10-15 15:20:18 +0800

    合并项目

---
+++ teamItem/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+		<zm-news></zm-news>
+		<hjy></hjy>
+		<syd></syd>
+  </div>
+</template>
+
+<script>
+import zmNews from './components/zmNews'
+import hjy from './components/hjy'
+import syd from './components/syd'
+export default {
+	name: 'App',
+	components: {
+		zmNews,
+		hjy,
+		syd
+	}
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app{
+	width: 100%;
+}
+</style>
